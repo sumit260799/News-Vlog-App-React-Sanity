f2175c372a157833e989be0b0a60feb8a508865b
Sumit Das <94750004+sumit260799@users.noreply.github.com>
2023-06-28 21:16:27 +0530

    Update oauth

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-crp10gjh3im7ulz4.us.auth0.com"
+    clientId="Rlwe63XFNdCxVNmLwtDBjJ825kObaefR"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>
 )
